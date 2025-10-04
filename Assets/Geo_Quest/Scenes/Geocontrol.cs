85cdb909e9f02fb1b68f3794664be22a5004acb8
REDmen17 <genrosswande@gmail.com>
2025-10-04 12:26:31 -0400

    10/4/2025

--- Assets/Geo_Quest/Scenes/Geocontrol.cs
+++ Assets/Geo_Quest/Scenes/Geocontrol.cs
@@ -3,16 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Geocontrol : MonoBehaviour
-{
+{private Rigidbody2D rb;
+ public int speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
+       
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = Vector2.left;
+        /*
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+      if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+
+        }
+        */
     }
 }
