cf076bb3ecc2208d0967f46a612265c5ab0636a8
REDmen17 <genrosswande@gmail.com>
2025-12-06 16:20:24 -0500

    12/6/25

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class HW2PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _Rigidbody2D;
+    
+    private float _xspeed; 
+    private float _yspeed;
 
+    private void Start()
+    {
+        _Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _xspeed = Input.GetAxis("Horizontal");
+        _yspeed = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(_xspeed, 0.0f, _yspeed);
+        _Rigidbody2D.AddForce(movement * 10);
+    }
 }
--- Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
@@ -2,5 +2,45 @@ using UnityEngine;
 
 public class HW2PlayerShoot : MonoBehaviour
 {
+    public GameObject prefab;
+    public Transform bulletTrash;
+    public Transform bulletSpam;
 
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canShoot = true;
+
+    private void Update()
+    {
+        if (!_canShoot)
+        {
+            TimerMethod();
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        {
+            GameObject bullet = Instantiate(prefab,
+                bulletSpam.position, bulletSpam.rotation, bulletTrash);
+            bullet.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
+        }
+    }
+
+    private void TimerMethod()
+    {
+        if (_canShoot)
+        {
+            _currentTime += Time.deltaTime;
+
+            if (_currentTime >= Timer)
+            {
+                _canShoot = true;
+                _currentTime = 0f;
+            }
+
+        }
+    }
 }
+      
